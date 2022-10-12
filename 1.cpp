ee2a6494cbe310628b492447f58ab3202336d02d
caiji666 <54731218+caiji666@users.noreply.github.com>
2022-10-12 18:48:40 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n>0 && n <11)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Hello,RoboMaster!"<< endl;
+        }
+    }
+    return 0;
+}
